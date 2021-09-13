0875baf4480bb019cdc7e73380047e831040dd49
StevenGann <Steven.Gann@Microchip.com>
2021-09-12 19:33:14 -0700

    Implemented Pipeline and Camera classes

---
+++ Common/Camera.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Numerics;
+using Silk.NET.Maths;
+using Silk.NET.Input;
+
+namespace Common
+{
+    public class Camera
+    {
+        private Vector3 m_pos;
+        private Vector3 m_target;
+        private Vector3 m_up;
+
+        private int m_windowWidth;
+        private int m_windowHeight;
+
+        private float m_AngleH;
+        private float m_AngleV;
+
+        private bool m_OnUpperEdge;
+        private bool m_OnLowerEdge;
+        private bool m_OnLeftEdge;
+        private bool m_OnRightEdge;
+
+        private Vector2D<int> m_mousePos;
+
+        private const float STEP_SCALE = 1.0f;
+        private const float EDGE_STEP = 0.5f;
+        private const int MARGIN = 10;
+
+        public Camera(int WindowWidth, int WindowHeight)
+        {
+            m_windowWidth = WindowWidth;
+            m_windowHeight = WindowHeight;
+            m_pos = new Vector3(0.0f, 0.0f, 0.0f);
+            m_target = new Vector3(0.0f, 0.0f, 1.0f);
+            //m_target.Normalize();
+            m_up = new Vector3(0.0f, 1.0f, 0.0f);
+
+            Init();
+        }
+
+        public Camera(int WindowWidth, int WindowHeight, Vector3 Pos, Vector3 Target, Vector3 Up)
+        {
+            m_windowWidth = WindowWidth;
+            m_windowHeight = WindowHeight;
+            m_pos = Pos;
+
+            m_target = Vector3.Normalize(Target);
+
+            m_up = Vector3.Normalize(Up);
+
+            Init();
+        }
+
+        public bool OnKeyboard(Key Key)
+        {
+            bool Ret = false;
+
+            switch (Key)
+            {
+                case Key.Up:
+                    {
+                        m_pos += (m_target * STEP_SCALE);
+                        Ret = true;
+                    }
+                    break;
+
+                case Key.Down:
+                    {
+                        m_pos -= (m_target * STEP_SCALE);
+                        Ret = true;
+                    }
+                    break;
+
+                case Key.Left:
+                    {
+                        Vector3 Left = Vector3.Normalize(Vector3.Cross(m_target, m_up));
+                        Left *= STEP_SCALE;
+                        m_pos += Left;
+                        Ret = true;
+                    }
+                    break;
+
+                case Key.Right:
+                    {
+                        Vector3 Right = Vector3.Normalize(Vector3.Cross(m_up, m_target));
+                        Right *= STEP_SCALE;
+                        m_pos += Right;
+                        Ret = true;
+                    }
+                    break;
+
+                case Key.PageUp:
+                    m_pos.Y += STEP_SCALE;
+                    break;
+
+                case Key.PageDown:
+                    m_pos.Y -= STEP_SCALE;
+                    break;
+
+                default:
+                    break;
+            }
+
+            return Ret;
+        }
+
+        public void OnMouse(int X, int Y)
+        {
+            int DeltaX = X - m_mousePos.X;
+            int DeltaY = Y - m_mousePos.Y;
+
+            m_mousePos.X = X;
+            m_mousePos.Y = Y;
+
+            m_AngleH += DeltaX / 20.0f;
+            m_AngleV += DeltaY / 20.0f;
+
+            if (DeltaX == 0)
+            {
+                if (X <= MARGIN)
+                {
+                    //    m_AngleH -= 1.0f;
+                    m_OnLeftEdge = true;
+                }
+                else if (X >= (m_windowWidth - MARGIN))
+                {
+                    //    m_AngleH += 1.0f;
+                    m_OnRightEdge = true;
+                }
+            }
+            else
+            {
+                m_OnLeftEdge = false;
+                m_OnRightEdge = false;
+            }
+
+            if (DeltaY == 0)
+            {
+                if (Y <= MARGIN)
+                {
+                    m_OnUpperEdge = true;
+                }
+                else if (Y >= (m_windowHeight - MARGIN))
+                {
+                    m_OnLowerEdge = true;
+                }
+            }
+            else
+            {
+                m_OnUpperEdge = false;
+                m_OnLowerEdge = false;
+            }
+
+            Update();
+        }
+
+        public void OnRender()
+        {
+            bool ShouldUpdate = false;
+
+            if (m_OnLeftEdge)
+            {
+                m_AngleH -= EDGE_STEP;
+                ShouldUpdate = true;
+            }
+            else if (m_OnRightEdge)
+            {
+                m_AngleH += EDGE_STEP;
+                ShouldUpdate = true;
+            }
+
+            if (m_OnUpperEdge)
+            {
+                if (m_AngleV > -90.0f)
+                {
+                    m_AngleV -= EDGE_STEP;
+                    ShouldUpdate = true;
+                }
+            }
+            else if (m_OnLowerEdge)
+            {
+                if (m_AngleV < 90.0f)
+                {
+                    m_AngleV += EDGE_STEP;
+                    ShouldUpdate = true;
+                }
+            }
+
+            if (ShouldUpdate)
+            {
+                Update();
+            }
+        }
+
+        public Vector3 GetPos()
+        {
+            return m_pos;
+        }
+
+        public Vector3 GetTarget()
+        {
+            return m_target;
+        }
+
+        public Vector3 GetUp()
+        {
+            return m_up;
+        }
+
+        public void AddToATB()//out TwBar bar)
+        {
+        }
+
+        private void Init()
+        {
+            Vector3 HTarget = Vector3.Normalize(new(m_target.X, 0.0f, m_target.Z));
+
+            if (HTarget.Z >= 0.0f)
+            {
+                if (HTarget.X >= 0.0f)
+                {
+                    m_AngleH = 360.0f - ToDegree(MathF.Asin(HTarget.Z));
+                }
+                else
+                {
+                    m_AngleH = 180.0f + ToDegree(MathF.Asin(HTarget.Z));
+                }
+            }
+            else
+            {
+                if (HTarget.X >= 0.0f)
+                {
+                    m_AngleH = ToDegree(MathF.Asin(-HTarget.Z));
+                }
+                else
+                {
+                    m_AngleH = 180.0f - ToDegree(MathF.Asin(-HTarget.Z));
+                }
+            }
+
+            m_AngleV = -ToDegree(MathF.Asin(m_target.Y));
+
+            m_OnUpperEdge = false;
+            m_OnLowerEdge = false;
+            m_OnLeftEdge = false;
+            m_OnRightEdge = false;
+            m_mousePos.X = m_windowWidth / 2;
+            m_mousePos.Y = m_windowHeight / 2;
+        }
+
+        private void Update()
+        {
+        }
+
+        private float ToRadian(float x)
+        {
+            return (float)(((x) * MathF.PI / 180.0f));
+        }
+
+        private float ToDegree(float x)
+        {
+            return (float)(((x) * 180.0f / MathF.PI));
+        }
+    }
+}
---
+++ Common/Pipeline.cs
@@ -0,0 +1,226 @@
+﻿using System.Numerics;
+
+namespace Common
+{
+    public struct PersProjInfo
+    {
+        public float FOV;
+        public float Width;
+        public float Height;
+        public float zNear;
+        public float zFar;
+    };
+
+    public struct OrthoProjInfo
+    {
+        public float r;        // right
+        public float l;        // left
+        public float b;        // bottom
+        public float t;        // top
+        public float n;        // z near
+        public float f;        // z far
+    };
+
+    public class Orientation
+    {
+        public Vector3 m_scale;
+        public Vector3 m_rotation;
+        public Vector3 m_pos;
+
+        private Orientation()
+        {
+            m_scale = new Vector3(1.0f, 1.0f, 1.0f);
+            m_rotation = new Vector3(0.0f, 0.0f, 0.0f);
+            m_pos = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    };
+
+    public class Pipeline
+    {
+        private Vector3 m_scale;
+        private Vector3 m_worldPos;
+        private Vector3 m_rotateInfo;
+
+        private PersProjInfo m_persProjInfo;
+        private OrthoProjInfo m_orthoProjInfo;
+
+        private Matrix4x4 m_WVPtransformation;
+        private Matrix4x4 m_VPtransformation;
+        private Matrix4x4 m_WPtransformation;
+        private Matrix4x4 m_WVtransformation;
+        private Matrix4x4 m_Wtransformation;
+        private Matrix4x4 m_Vtransformation;
+        private Matrix4x4 m_ProjTransformation;
+
+        public struct CameraPosition
+        {
+            public Vector3 Pos;
+            public Vector3 Target;
+            public Vector3 Up;
+        }
+
+        private CameraPosition m_camera;
+
+        public Pipeline()
+        {
+            m_scale = new Vector3(1.0f, 1.0f, 1.0f);
+            m_worldPos = new Vector3(0.0f, 0.0f, 0.0f);
+            m_rotateInfo = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+
+        public void Scale(float s)
+        {
+            Scale(s, s, s);
+        }
+
+        public void Scale(Vector3 scale)
+        {
+            Scale(scale.X, scale.Y, scale.Z);
+        }
+
+        public void Scale(float ScaleX, float ScaleY, float ScaleZ)
+        {
+            m_scale.X = ScaleX;
+            m_scale.Y = ScaleY;
+            m_scale.Z = ScaleZ;
+        }
+
+        public void WorldPos(float x, float y, float z)
+        {
+            m_worldPos.X = x;
+            m_worldPos.Y = y;
+            m_worldPos.Z = z;
+        }
+
+        public void WorldPos(Vector3 Pos)
+        {
+            m_worldPos = Pos;
+        }
+
+        public void Rotate(float RotateX, float RotateY, float RotateZ)
+        {
+            m_rotateInfo.X = RotateX;
+            m_rotateInfo.Y = RotateY;
+            m_rotateInfo.Z = RotateZ;
+        }
+
+        public void Rotate(Vector3 r)
+        {
+            Rotate(r.X, r.Y, r.Z);
+        }
+
+        public void SetPerspectiveProj(PersProjInfo p)
+        {
+            m_persProjInfo = p;
+        }
+
+        public void SetOrthographicProj(OrthoProjInfo p)
+        {
+            m_orthoProjInfo = p;
+        }
+
+        public void SetCamera(Vector3 Pos, Vector3 Target, Vector3 Up)
+        {
+            m_camera.Pos = Pos;
+            m_camera.Target = Target;
+            m_camera.Up = Up;
+        }
+
+        private void SetCamera(Camera camera)
+        {
+            SetCamera(camera.GetPos(), camera.GetTarget(), camera.GetUp());
+        }
+
+        private void Orient(Orientation o)
+        {
+            m_scale = o.m_scale;
+            m_worldPos = o.m_pos;
+            m_rotateInfo = o.m_rotation;
+        }
+
+        public Matrix4x4 GetProjTrans()
+        {
+            m_ProjTransformation = Matrix4x4.CreatePerspective(
+                m_persProjInfo.Width,
+                m_persProjInfo.Height,
+                m_persProjInfo.zNear,
+                m_persProjInfo.zFar);
+            return m_ProjTransformation;
+        }
+
+        public Matrix4x4 GetVPTrans()
+        {
+            GetViewTrans();
+            GetProjTrans();
+
+            m_VPtransformation = m_ProjTransformation * m_Vtransformation;
+            return m_VPtransformation;
+        }
+
+        public Matrix4x4 GetWorldTrans()
+        {
+            Matrix4x4 ScaleTrans = Matrix4x4.CreateScale(m_scale.X, m_scale.Y, m_scale.Z);
+            Matrix4x4 RotateTrans = Matrix4x4.CreateFromYawPitchRoll(m_rotateInfo.X, m_rotateInfo.Y, m_rotateInfo.Z);
+            Matrix4x4 TranslationTrans = Matrix4x4.CreateTranslation(m_worldPos.X, m_worldPos.Y, m_worldPos.Z);
+
+            m_Wtransformation = TranslationTrans * RotateTrans * ScaleTrans;
+            return m_Wtransformation;
+        }
+
+        public Matrix4x4 GetViewTrans()
+        {
+            m_Vtransformation = Matrix4x4.CreateLookAt(m_camera.Pos, m_camera.Target, m_camera.Up);
+
+            return m_Vtransformation;
+        }
+
+        public Matrix4x4 GetWVPTrans()
+        {
+            GetWorldTrans();
+            GetVPTrans();
+
+            m_WVPtransformation = m_VPtransformation * m_Wtransformation;
+            return m_WVPtransformation;
+        }
+
+        public Matrix4x4 GetWVOrthoPTrans()
+        {
+            GetWorldTrans();
+            GetViewTrans();
+
+            Matrix4x4 P = Matrix4x4.CreateOrthographicOffCenter(
+                m_orthoProjInfo.l,
+                m_orthoProjInfo.r,
+                m_orthoProjInfo.b,
+                m_orthoProjInfo.t,
+                m_orthoProjInfo.n,
+                m_orthoProjInfo.f);
+
+            m_WVPtransformation = P * m_Vtransformation * m_Wtransformation;
+            return m_WVPtransformation;
+        }
+
+        public Matrix4x4 GetWVTrans()
+        {
+            GetWorldTrans();
+            GetViewTrans();
+
+            m_WVtransformation = m_Vtransformation * m_Wtransformation;
+            return m_WVtransformation;
+        }
+
+        public Matrix4x4 GetWPTrans()
+        {
+            Matrix4x4 PersProjTrans = Matrix4x4.CreatePerspective(
+                m_persProjInfo.Width,
+                m_persProjInfo.Height,
+                m_persProjInfo.zNear,
+                m_persProjInfo.zFar);
+
+            GetWorldTrans();
+
+            m_WPtransformation = PersProjTrans * m_Wtransformation;
+            return m_WPtransformation;
+        }
+    };
+}
